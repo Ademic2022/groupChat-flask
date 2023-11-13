b99b57f82b692b368a6ec85669f6c601297c864b
“Ademic” <adeeyomichael77@gmail.com>
2023-11-13 16:20:50 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO
 import random
+from flask_login import LoginManager, logout_user, login_user, login_required, current_user
 from string import ascii_uppercase
 from socketIo.socketIoEngine import ChatSocketIO
+from models.storage.storage import get_user
 
 app = Flask(__name__)
 rooms = {}
 app.config['SECRET_KEY'] = 'dvhdnmfgavregancb'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 socketio = SocketIO(app)
 sio = ChatSocketIO(socketio, rooms)
 
@@ -19,10 +27,20 @@ def generate_unique_code(length):
             break
     return code
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # session.clear()
     if request.method == 'POST':
-        pass
+        username = request.form.get('username').strip()
+        password = request.form.get('password').strip()
+
+        user = get_user(username)
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('index'))
+        
+        return redirect(url_for('login'))
 
     return render_template('login.html')
 
@@ -34,11 +52,12 @@ def signup():
     return render_template('signup.html')
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def index():
-    session.clear()
+    # session.pop('room', None)
     if request.method == 'POST':
-        profileName = request.form.get('profileName')
-        code = request.form.get('code')
+        profileName = current_user.username
+        code = request.form.get('code').strip()
         joinRoom = request.form.get('joinRoom', False)
         createRoom = request.form.get('createRoom', False)
         if not profileName:
@@ -60,15 +79,31 @@ def index():
     
     return render_template('index.html')
 
-@app.route('/groupchat', methods=['POST', 'GET'])
-def groupchat():
+@app.route('/groupchat/<room_id>', methods=['POST', 'GET'])
+@app.route('/groupchat', defaults={'room_id': None}, methods=['POST', 'GET'])
+# @app.route('/groupchat', methods=['POST', 'GET'])
+@login_required
+def groupchat(room_id):
+    if room_id is not None:
+        room_id = room_id.upper()
+        print(room_id)
     room = session.get('room')
-    profileName = session.get('profileName')
-    if room not in rooms or room is None or profileName is None:
+    if room not in rooms or room is None:
+        print("Redirecting to index - Room not in rooms or is None")
+        return redirect(url_for('index'))
+    if room_id is not None and room_id != room:
+        print(f"Redirecting to index - room_id mismatch: {room_id} != {room}")
         return redirect(url_for('index'))
     print(room)
 
     return render_template('groupchat.html', room=room, messages=rooms[room]['messages'])
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 
 @socketio.on('message')
 def message(data):
