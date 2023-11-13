99add52c5a20febd861d65aa1dcbf60e3bf70c3d
“Ademic” <adeeyomichael77@gmail.com>
2023-11-13 16:21:17 +0100

    user objects

--- models/users.py
+++ models/users.py
@@ -1,16 +1,28 @@
 import pymongo
 from hashlib import md5
+from flask_login import UserMixin
 
 
-class User:
+class User(UserMixin):
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = password
     
     def check_password(self, password):
-        pass
+        encoded_password = md5(password.encode()).hexdigest()
+        if self.password == encoded_password:
+            return True
+        return False
     @staticmethod
-    def isActive(self):
+    def is_active(self):
         return True
     
+    def get_id(self):
+        return self.username
+    @staticmethod
+    def is_authenticated(self):
+        return True
+    @staticmethod
+    def is_anonymous(self):
+        return False
