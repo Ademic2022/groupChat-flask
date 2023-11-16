11ef0a220a720ff9b721c546164920a08ddb9679
“Ademic” <adeeyomichael77@gmail.com>
2023-11-16 09:05:51 +0100

    added more features

--- static/javascript/base.js
+++ static/javascript/base.js
@@ -1,13 +1,20 @@
-const socket = io();
+const socket = io.connect('http://127.0.0.1:5008/')
 const messages = document.getElementById('messages');
+const username = document.getElementById('hiddenName').value
+const roomId = document.getElementById('roomId').value
+
 
 const createMessage = (profileName, msg) => {
+    const isCurrentUser = profileName === username
+    const textColor = isCurrentUser ? 'green' : 'red';
+    console.log('createMessage called:', profileName, msg, isCurrentUser);
+
     const content = `
         <div class="message-content">
             <span>
-                <strong>${profileName}</strong>: ${msg}
+                <strong style="color: ${textColor};">${profileName}</strong>: ${msg}
             </span>
-            <span class="muted">
+            <span class="muted" style="color: ${textColor};">
                 ${new Date().toLocaleString()}
             </span>
         </div>
@@ -15,15 +22,45 @@ const createMessage = (profileName, msg) => {
     messages.innerHTML += content;
 };
 
-socket.on('message', (data) => {
-    createMessage(data.profileName, data.message);
+socket.on('connect', () => {
+    socket.emit('join_room', {
+        username: username,
+        room: roomId
+    });
 });
 
 const sendMessage = () => {
     const message = document.getElementById('message');
-    if (message.value === '') return;
-    socket.emit('message', { data: message.value });
+    messageInput = message.value.trim()
+    if (messageInput === '') return;
+    socket.emit('send_message', { 'username': username, 'message': messageInput, 'room': roomId});
     message.value = '';
+    message.focus();
 };
 
-// export { createMessage };
+window.onbeforeunload = () => {
+    socket.emit('leave_room', {
+        username: username,
+        room: roomId
+    })
+};
+
+socket.on('receive_message', (data) => {
+    createMessage(data.username, data.message);
+});
+
+socket.on('join_room_announcement', (data) => {
+    console.log(data);
+    if (data.username !== username) {
+        message = `<b>${data.username}</b> has joined the room`
+        createMessage(data.username, message);
+    }
+});
+
+socket.on('leave_room_announcement', function (data) {
+    console.log(data);
+    message = `<b>${data.username}</b> has left the room`
+    createMessage(data.username, message);
+
+    
+});
