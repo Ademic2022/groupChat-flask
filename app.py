f1cc10d08eaf1ceb8b353b0db4061ec137a6a0c8
“Ademic” <adeeyomichael77@gmail.com>
2023-11-16 09:03:27 +0100

    code reformatted, realtime chat implemented

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
 import random
+from flask_socketio import rooms
 from flask_login import LoginManager, logout_user, login_user, login_required, current_user
 from string import ascii_uppercase
 from socketIo.socketIoEngine import ChatSocketIO
-from models.storage.storage import get_user
+from models.storage.storage import MongoDBConnection
 
 app = Flask(__name__)
 rooms = {}
@@ -12,11 +13,14 @@ app.config['SECRET_KEY'] = 'dvhdnmfgavregancb'
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+db = MongoDBConnection()
+
+
 @login_manager.user_loader
 def load_user(username):
-    return get_user(username)
+    return db.get_user(username)
 socketio = SocketIO(app)
-sio = ChatSocketIO(socketio, rooms)
+sio = ChatSocketIO(app, socketio)
 
 def generate_unique_code(length):
     while True:
@@ -30,12 +34,14 @@ def generate_unique_code(length):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     # session.clear()
     if request.method == 'POST':
         username = request.form.get('username').strip()
         password = request.form.get('password').strip()
 
-        user = get_user(username)
+        user = db.get_user(username)
         if user and user.check_password(password):
             login_user(user)
             return redirect(url_for('index'))
@@ -46,57 +52,84 @@ def login():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if request.method == 'POST':
-        pass
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
+    if request.method == 'POST':
+        username = request.form.get('username').strip()
+        email = request.form.get('email').strip()
+        password = request.form.get('password').strip()
+
+        user = db.get_user(username)
+        if not user:
+            """save new user"""
+            db.save_user(username,email,password)
+            return redirect(url_for('login'))
+        
+        return render_template('signup.html')
+      
     return render_template('signup.html')
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    session.pop(current_user.username, None)
     if request.method == 'POST':
-        profileName = current_user.username
-        code = request.form.get('code').strip()
-        joinRoom = request.form.get('joinRoom', False)
+        profileName = request.form.get('profileName').strip()
         createRoom = request.form.get('createRoom', False)
+
         if not profileName:
             return render_template('index.html', error='enter a profile name')
 
-        if joinRoom != False and not code:
-            return render_template('index.html', error='enter roomCode', profileName=profileName, code=code)
-        room = code
+        for key in request.form.keys():
+            if key.startswith('joinRoom_'):
+                room_id = key.split('_')[1]
+                print(f"Joining room with ID {room_id}")
+                return redirect(url_for('groupchat', room_id = room_id))
+            
 
         if createRoom != False:
+            # usernames = [username.strip() for username in request.form.get('members').split(',')]
+            usernames = [username.strip() for username in []]
             room = generate_unique_code(4)
-            rooms[room] = {'members':0, 'messages':[]}
-        elif code not in rooms:
-            return render_template('index.html', error='room does not exist', profileName=profileName, code=code)
 
-        session['room'] = room 
-        session['profileName'] = profileName
+            """Save Room and Insert bulk members to the Room"""
+            if len(usernames):
+                room_id = db.save_room(room, current_user.username)
+                if current_user.username in usernames:
+                    usernames.remove(current_user.username)
+                db.add_room_members(room_id, room, usernames, current_user.username)
+
+                return redirect(url_for('groupchat', room_id=room_id))
+
+            """Save Room and Insert Self as Room Member"""
+            room_id = db.save_room(room, current_user.username)
+            return redirect(url_for('groupchat', room_id=room_id))
+
         return redirect(url_for('groupchat'))
     
-    return render_template('index.html')
+    rooms = []
+    if current_user.is_authenticated:
+        rooms = db.get_rooms_for_user(current_user.username)
+    return render_template('index.html', rooms=rooms)
 
 @app.route('/groupchat/<room_id>', methods=['POST', 'GET'])
 @app.route('/groupchat', defaults={'room_id': None}, methods=['POST', 'GET'])
-# @app.route('/groupchat', methods=['POST', 'GET'])
 @login_required
 def groupchat(room_id):
-    if room_id is not None:
-        room_id = room_id.upper()
-        print(room_id)
-    room = session.get('room')
-    if room not in rooms or room is None:
-        print("Redirecting to index - Room not in rooms or is None")
-        return redirect(url_for('index'))
-    if room_id is not None and room_id != room:
-        print(f"Redirecting to index - room_id mismatch: {room_id} != {room}")
-        return redirect(url_for('index'))
-    print(room)
+    if room_id is None:
+        # flash('Please select a valid room.')
+        return redirect(url_for('index'))  # Replace with your default route
 
-    return render_template('groupchat.html', room=room, messages=rooms[room]['messages'])
+    room = db.get_room(room_id)
+    room_member = db.is_room_member(room_id, current_user.username)
+
+    if room and room_member:
+        room_members = db.get_room_members(room_id)
+        return render_template('groupchat.html', room=room, room_members=room_members)
+
+    # flash('Invalid room or you are not a member.')
+    return redirect(url_for('index'))
+
 
 @app.route("/logout")
 @login_required
@@ -105,17 +138,19 @@ def logout():
     return redirect(url_for('login'))
 
 
-@socketio.on('message')
-def message(data):
-    sio.create_message(data)
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    sio.handle_send_message_event(data)
 
-@socketio.on('connect')
-def connects(auth):
-    sio.handle_connect(auth)
 
-@socketio.on('disconnect')
-def disconnect():
-    sio.handle_disconnect()
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    sio.handle_join_room_event(data)
+
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    sio.handle_leave_room_event(data)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5008)
