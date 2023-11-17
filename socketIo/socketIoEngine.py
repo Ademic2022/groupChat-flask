84a757e9155d3e39dde6204725e0406443384366
“Ademic” <adeeyomichael77@gmail.com>
2023-11-17 15:07:31 +0100

    websocket

--- socketIo/socketIoEngine.py
+++ socketIo/socketIoEngine.py
@@ -1,4 +1,7 @@
-from flask_socketio import join_room, leave_room, close_room, rooms
+from flask_socketio import join_room, leave_room
+from models.storage.storage import MongoDBConnection
+from datetime import datetime
+db = MongoDBConnection()
 
 class ChatSocketIO:
 
@@ -8,14 +11,18 @@ class ChatSocketIO:
 
     def handle_send_message_event(self, data):
         self.app.logger.info(f"{data['username']} has sent a message to the room {data['room']}: {data['message']}")
+        data['created_at'] = datetime.now().strftime(("%d %b, %H:%M:%S"))
+        db.save_messages(data['room'], data['message'], data['username'])
         self.socketio.emit('receive_message', data, room=data['room'])
 
     def handle_join_room_event(self, data):
         self.app.logger.info(f"{data['username']} has joined the room {data['room']}")
         join_room(data['room'])
+        data['created_at'] = datetime.now().strftime(("%d %b, %H:%M:%S"))
         self.socketio.emit('join_room_announcement', data, room=data['room'])
 
     def handle_leave_room_event(self, data):
         self.app.logger.info(f"{data['username']} has left the room {data['room']}")
         leave_room(data['room'])
+        data['created_at'] = datetime.now().strftime(("%d %b, %H:%M:%S"))
         self.socketio.emit('leave_room_announcement', data, room=data['room'])
