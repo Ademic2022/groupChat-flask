110413916acb6c6fbe23d64a3bd1f996e7a82d88
“Ademic” <adeeyomichael77@gmail.com>
2023-11-21 18:35:28 +0100

    improving ui

--- static/javascript/chatRoom.js
+++ static/javascript/chatRoom.js
@@ -96,3 +96,278 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+const handleItemClick = (element)=>{
+    const tempTemplate = document.getElementById('tempTemplate')
+    const username = document.getElementById('hiddenName').value
+    tempTemplate.classList.add('hide')
+
+
+    var roomId = element.getAttribute('data-roomId');
+    console.log('Item clicked with roomId:', roomId);
+    const messages = document.getElementById('messages');
+    
+    initializeSocketConnection(username, roomId);
+    $.ajax({
+        type: 'POST',
+        url: '/groupchat/' + roomId,
+        success: function(response) {
+            // Handle success (if needed)
+            console.log('POST request successful:', response);
+            document.getElementById('roomTitle').textContent = response.room.name
+            messages.innerHTML =''
+            if (Array.isArray(response.messages)) {
+                response.messages.reverse().forEach(message => {
+                    appendMessages(message.text, message.sender, message.created_at)
+                });
+            } else {
+                console.error('Unexpected response format:', response);
+            }
+        },
+        error: function(error) {
+            // Handle error (if needed)
+            console.error('Error in POST request:', error);
+        }
+    });
+
+
+
+    let page = 0
+    const loadPreviousMessages = document.getElementById('loadPreviousMessages')
+    loadPreviousMessages.addEventListener('click', (e)=>{
+        page += 1
+        // const roomId = document.getElementById('roomId')
+        var roomId = element.getAttribute('data-roomId');
+        console.log(roomId);
+        const url = `/rooms/${roomId}/messages?page=${page}`
+        e.preventDefault();
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response =>{
+            response.json().then(messages => {
+                
+                messages.reverse().forEach(message => prependMessages(message.text, message.sender, message.created_at))
+            })
+        })
+    })
+
+}
+
+
+
+const prependMessages = (message, username, created_at) =>{
+    const profileName = document.getElementById('hiddenName').value
+    const isCurrentUser = profileName === username
+    if (isCurrentUser){
+        const content = `
+            <div class="message-block">
+                <div class="message-block-sidebar">
+                   <p>A</p>
+                </div>
+                <div class="message-block-content">
+                    <p class="message"></p>
+                    <p class="message-block-content-name" style="color: green;">~ 
+                        <i>${username}</i>
+                    </p>
+                    <p class="text-message">${message}</p>
+                    <span class="senders-time"><i>${created_at}</i></span>
+                </div>
+            </div>
+        `;
+        // insertAdjacentHTML with 'afterbegin' to prepend the content
+        messages.insertAdjacentHTML('afterbegin', content);
+    }
+    else{
+        const content = `
+
+            <div class="message-block receiver-block">
+                <div class="message-block-content" style="border-radius: 10px 0 10px 10px;">
+                    <p class="message-receiver"></p>
+                    <p class="message-block-content-name-receiver" style="color: red;">~ 
+                        <i>${username}</i>
+                    </p>
+                    <p class="text-message"> ${message}</p>
+                    <span class="receivers-time"><i>${created_at}</i></span>
+                </div>
+                <div class="message-block-sidebar">
+                    <p>G</p>
+                </div>
+            </div>
+        `;
+        // insertAdjacentHTML with 'afterbegin' to prepend the content
+        messages.insertAdjacentHTML('afterbegin', content);
+    }
+    
+}
+
+const appendMessages = (message, username, created_at) =>{
+    const profileName = document.getElementById('hiddenName').value
+    const isCurrentUser = profileName === username
+    if (isCurrentUser){
+        const content = `
+            <div class="message-block">
+                <div class="message-block-sidebar">
+                   <p>A</p>
+                </div>
+                <div class="message-block-content">
+                    <p class="message"></p>
+                    <p class="message-block-content-name" style="color: green;">~ 
+                        <i>${username}</i>
+                    </p>
+                    <p class="text-message">${message}</p>
+                    <span class="senders-time"><i>${created_at}</i></span>
+                </div>
+            </div>
+        `;
+        // insertAdjacentHTML with 'afterbegin' to prepend the content
+        messages.insertAdjacentHTML('afterbegin', content);
+    }
+    else{
+        const content = `
+
+            <div class="message-block receiver-block">
+                <div class="message-block-content" style="border-radius: 10px 0 10px 10px;">
+                    <p class="message-receiver"></p>
+                    <p class="message-block-content-name-receiver" style="color: red;">~ 
+                        <i>${username}</i>
+                    </p>
+                    <p class="text-message"> ${message}</p>
+                    <span class="receivers-time"><i>${created_at}</i></span>
+                </div>
+                <div class="message-block-sidebar">
+                    <p>G</p>
+                </div>
+            </div>
+        `;
+        // insertAdjacentHTML with 'afterbegin' to prepend the content
+        messages.insertAdjacentHTML('beforeend', content);
+    }
+    
+}
+
+
+
+
+
+
+
+const createMessage = (profileName, msg, created_at) => {
+    const isCurrentUser = profileName === username
+    const textColor = isCurrentUser ? 'green' : 'red';
+    console.log('createMessage called:', profileName, msg, isCurrentUser);
+    
+    const content = `
+    <div class="message-content">
+    <span>
+    <strong style="color: ${textColor};">${profileName}</strong>: ${msg}
+    </span>
+    <span class="muted" style="color: ${textColor};">
+    ${created_at}
+    </span>
+    </div>
+    `;
+    messages.innerHTML += content;
+};
+
+
+
+const sendMessage = () => {
+    const message = document.getElementById('message');
+    console.log('message sent');
+    messageInput = message.value.trim()
+    if (messageInput === '') return;
+    socket.emit('send_message', { 'username': username, 'message': messageInput, 'room': roomId});
+    message.value = '';
+    message.focus();
+};
+
+// // ================================ SOCKET CONNECTION =========================== //
+// const socket = io.connect('http://127.0.0.1:5008/')
+
+// socket.on('connect', () => {
+//     socket.emit('join_room', {
+//         username: username,
+//         room: roomId
+//     });
+// });
+
+
+// window.onbeforeunload = () => {
+//     socket.emit('leave_room', {
+//         username: username,
+//         room: roomId
+//     })
+// };
+
+// socket.on('receive_message', (data) => {
+//     createMessage(data.username, data.message, data.created_at);
+// });
+
+// socket.on('join_room_announcement', (data) => {
+//     console.log(data);
+
+//     if (data.username !== username) {
+//         message = `<b>${data.username}</b> has joined the room`
+//         createMessage(data.username, message, data.created_at);
+//     }
+// });
+
+// socket.on('leave_room_announcement', function (data) {
+//     console.log(data);
+//     message = `<b>${data.username}</b> has left the room`
+//     createMessage(data.username, message, data.created_at);   
+// });
+
+function initializeSocketConnection(username, roomId) {
+    const socket = io.connect('http://127.0.0.1:5008/');
+
+    socket.on('connect', () => {
+        socket.emit('join_room', {
+            username: username,
+            room: roomId
+        });
+    });
+
+    window.onbeforeunload = () => {
+        socket.emit('leave_room', {
+            username: username,
+            room: roomId
+        });
+    };
+
+    socket.on('receive_message', (data) => {
+        createMessage(data.username, data.message, data.created_at);
+    });
+
+    socket.on('join_room_announcement', (data) => {
+        console.log(data);
+
+        if (data.username !== username) {
+            message = `<b>${data.username}</b> has joined the room`;
+            createMessage(data.username, message, data.created_at);
+        }
+    });
+
+    socket.on('leave_room_announcement', function (data) {
+        console.log(data);
+        message = `<b>${data.username}</b> has left the room`;
+        createMessage(data.username, message, data.created_at);
+    });
+}
+
+
+
