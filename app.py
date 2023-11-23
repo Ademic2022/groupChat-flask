f157f682cb4b6b5fe1fca94c7b517ec6ce46c320
“Ademic” <adeeyomichael77@gmail.com>
2023-11-23 15:19:44 +0100

    add new Group function added

--- app.py
+++ app.py
@@ -71,37 +71,21 @@ def signup():
 @login_required
 def index():
     if request.method == 'POST':
-        profileName = request.form.get('profileName').strip()
-        createRoom = request.form.get('createRoom', False)
+        room_name = request.form.get('groupName')
+        usernames = [username.strip() for username in request.form.get('groupMembers').split(',')]
 
-        if not profileName:
-            return render_template('index.html', error='enter a profile name')
+        """Save Room and Insert bulk members to the Room"""
+        if len(room_name) and len(usernames):
+            room_id = db.save_room(room_name, current_user.username)
+            if current_user.username in usernames:
+                usernames.remove(current_user.username)
+            db.add_room_members(room_id, room_name, usernames, current_user.username)
 
-        for key in request.form.keys():
-            if key.startswith('joinRoom_'):
-                room_id = key.split('_')[1]
-                print(f"Joining room with ID {room_id}")
-                return redirect(url_for('groupchat', room_id = room_id))
-            
-
-        if createRoom != False:
-            room_name = request.form.get('room_name')
-            usernames = [username.strip() for username in request.form.get('members').split(',')]
-
-            """Save Room and Insert bulk members to the Room"""
-            if len(room_name) and len(usernames):
-                room_id = db.save_room(room_name, current_user.username)
-                if current_user.username in usernames:
-                    usernames.remove(current_user.username)
-                db.add_room_members(room_id, room_name, usernames, current_user.username)
-
-                return redirect(url_for('groupchat', room_id=room_id))
-            else:
-                """Save Room and Insert Self as Room Member"""
-                room_id = db.save_room(room_name, current_user.username)
-                return redirect(url_for('groupchat', room_id=room_id))
-
-        return redirect(url_for('groupchat'))
+            return redirect(url_for('groupchat', room_id=room_id))
+        else:
+            """Save Room and Insert Self as Room Member"""
+            room_id = db.save_room(room_name, current_user.username)
+            return redirect(url_for('groupchat', room_id=room_id))
     
     rooms = []
     if current_user.is_authenticated:
@@ -137,24 +121,6 @@ def edit_room(room_id):
     else:
         return "Only Admin can edit this Room", 404
 
-# """ CHAT ROOM """
-# @app.route('/groupchat/<room_id>', methods=['POST', 'GET'])
-# @app.route('/groupchat', defaults={'room_id': None}, methods=['POST', 'GET'])
-# @login_required
-# def groupchat(room_id):
-#     if room_id is None:
-#         return redirect(url_for('index'))
-
-#     room = db.get_room(room_id)
-#     room_member = db.is_room_member(room_id, current_user.username)
-
-#     if room and room_member:
-#         room_members = db.get_room_members(room_id)
-#         messages = db.get_messages(room_id)
-#         return render_template('groupchat.html', room=room, room_members=room_members, messages=messages)
-
-#     # flash('Invalid room or you are not a member.')
-#     return redirect(url_for('index'))
 
 @app.route('/groupchat/<room_id>', methods=['POST', 'GET'])
 @app.route('/groupchat', defaults={'room_id': None}, methods=['POST', 'GET'])
